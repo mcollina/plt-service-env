a120623bfcd6f8150a8f54a99b89ff571a4818b4
Matteo Collina <hello@matteocollina.com>
2023-10-06 16:56:20 +0200

    some initial code
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

---
+++ api/plugins/example.js
@@ -0,0 +1,7 @@
+/// <reference path="../global.d.ts" />
+'use strict'
+/** @param {import('fastify').FastifyInstance} fastify */
+module.exports = async function (fastify, opts) {
+  fastify.decorate('example', 'foobar')
+  fastify.decorate('greeting', opts.greeting)
+}
